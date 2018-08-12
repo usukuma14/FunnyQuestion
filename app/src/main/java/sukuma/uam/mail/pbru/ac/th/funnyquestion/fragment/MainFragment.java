01c1712c21c73755eb0ea51a04cd1009f04ae6f0
sukuma <sukuma99@gmail.com>
2018-08-12 13:53:11 +0700

    Replace and Pop Fragment

--- app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/fragment/MainFragment.java
+++ app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/fragment/MainFragment.java
@@ -7,10 +7,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import sukuma.uam.mail.pbru.ac.th.funnyquestion.R;
 
 public class MainFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+
+    }    //Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Raplace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragent, new ServiceFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
---
+++ app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/fragment/ServiceFragment.java
@@ -0,0 +1,48 @@
+package sukuma.uam.mail.pbru.ac.th.funnyquestion.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sukuma.uam.mail.pbru.ac.th.funnyquestion.MainActivity;
+import sukuma.uam.mail.pbru.ac.th.funnyquestion.R;
+
+public class ServiceFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        crete toolbar
+        creteToolbar();
+
+    }   //MainMethod
+
+    private void creteToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill Every Blank");
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        return view;
+    }
+}
