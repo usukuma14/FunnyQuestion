27128906aa809bbd5e2d4a28bf0f3a33ba017b49
sukuma <sukuma99@gmail.com>
2018-08-13 11:34:57 +0700

    Authen Success

--- app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/ServiceActivity.java
+++ app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/ServiceActivity.java
@@ -2,12 +2,72 @@ package sukuma.uam.mail.pbru.ac.th.funnyquestion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class ServiceActivity extends AppCompatActivity {
+
+    private String nameUserStringString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+
+//        Find User
+        findUser();
+
+
+//        Create Toolbar
+        createToolbar();
+
+
+    } //Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemExit) {
+            MyExit();
+        }
+        return super.onOptionsItemSelected(item);
     }
-}
+
+    private void MyExit() {
+//        Sign out
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+
+//        Exit app
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_service, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void findUser() {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        nameUserStringString = firebaseUser.getDisplayName();
+
+    }
+
+    private void createToolbar() {
+        Toolbar toolbar = findViewById(R.id.toobarService);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setSubtitle("Welcome" + nameUserStringString);
+    }
+
+} //Main Class
+
+
--- app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/fragment/MainFragment.java
+++ app/src/main/java/sukuma/uam/mail/pbru/ac/th/funnyquestion/fragment/MainFragment.java
@@ -1,5 +1,6 @@
 package sukuma.uam.mail.pbru.ac.th.funnyquestion.fragment;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,13 +9,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import sukuma.uam.mail.pbru.ac.th.funnyquestion.R;
 import sukuma.uam.mail.pbru.ac.th.funnyquestion.ServiceActivity;
+import sukuma.uam.mail.pbru.ac.th.funnyquestion.utility.MyAlertDialog;
 
 public class MainFragment extends Fragment {
 
@@ -29,8 +36,62 @@ public class MainFragment extends Fragment {
 //        Register Controller
         registerController();
 
+//        Login Contoller
+        loginContoller();
+
 
     }    //Main Method
+
+    private void loginContoller() {
+        Button button = getView().findViewById(R.id.btnlogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.editPassword);
+
+                String emailString = emailEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                    MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                    myAlertDialog.normalDialog("Have Space", "Please Fill All Blank");
+//                    True
+                } else {
+                    checkEmailAnPass(emailString, passwordString);
+
+                }
+
+            }
+        });
+    }
+
+    private void checkEmailAnPass(String emailString, String passwordString) {
+
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Check Authentication");
+        progressDialog.setMessage("Please wait for a few minute");
+        progressDialog.show();
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            moveToService();
+                        } else {
+                            progressDialog.dismiss();
+                            MyAlertDialog myAlertDialog = new MyAlertDialog(getActivity());
+                            myAlertDialog.normalDialog("Cannot Authen");
+                            task.getException().getMessage();
+
+                        }
+                    }
+                });
+    }
 
     private void checkStatus() {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
